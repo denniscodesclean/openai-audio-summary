58d7ede8d3454d517f86c50a7fa8fcb25d174d21
Delin Shen <dennis@DelindeMacBook-Air.local>
2024-12-28 23:01:06 -0800

    initial prompt

---
+++ audio-summary.py
@@ -0,0 +1,22 @@
+from openai import OpenAI 
+
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the audio.wav file
+audio_file = open('audio.wav', "rb")
+
+# Create a transcription request using audio_file
+audio_response = client.audio.transcriptions.create(
+    model='whisper-1',
+    file=audio_file
+)
+
+# Create a request to the API to identify the language spoken
+chat_response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=[
+        {'role':'system','content':'You are a helpful text sumamrize assistant. First identify the language of the text at the beginning, then use English to summarize the key points in some sentences'},
+        {'role':'user','content':audio_response.text}
+    ]
+)
+print(chat_response.choices[0].message.content)
